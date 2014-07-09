4fe71d24011d449e62d04dcae1d8149834a36276
Mike DePaulo <mikedep333@gmail.com>
2014-07-08 22:13:15 -0400

    Make the jenkins builds fail if the actual build commands failed

--- bin/nsis-builder.bat
+++ bin/nsis-builder.bat
@@ -8,8 +8,8 @@ cd D:\Build\GIT\nightly\x2goclient
 rem use msysgit's sed
 rem enable debug
 if "%1"=="--console" sed -i 's/#CONFIG += console/CONFIG += console/' x2goclient.pro
-%COMSPEC% /c config_win.bat
-mingw32-make
+%COMSPEC% /c config_win.bat || exit /b %errorlevel%
+mingw32-make || exit /b %errorlevel%
 dir release\x2goclient.exe
 rmdir /s /q nsis\x2goclient
 cd nsis
@@ -17,6 +17,6 @@ mkdir x2goclient
 xcopy /S D:\Build\scripts\current_files\x2goclient x2goclient
 copy ..\release\x2goclient.exe x2goclient\
 upx x2goclient\x2goclient.exe
-makensis x2goclient.nsi
+makensis x2goclient.nsi || exit /b %errorlevel%
 
 
