a1c3e517f3e709b3f5e14eeba8caa93504d4715a
Mike DePaulo <mikedep333@gmail.com>
2014-09-13 11:20:10 -0400

    Add git short hash to the version string (So that earlier nightly builds from the same day are not overwritten)

--- bin/generate-nsis-version.pl
+++ bin/generate-nsis-version.pl
@@ -13,6 +13,7 @@ close(F);
 my $text=join("",@lines);
 my $version=(split("VERSION \"",$text))[1];
 my $version=(split("\"",$version))[0];
+my $hash=`cd $ARGV[0] && git log --pretty=format:'%h' -n 1`;
 
 open(F,"<$ARGV[0]/nsis/x2goclient.nsi") or die;
 @lines=<F>;
@@ -20,7 +21,7 @@ close(F);
 
 $text=join("",@lines);
 
-$text=~s/X2GOCLIENT_VERSION/$version-$date/;
+$text=~s/X2GOCLIENT_VERSION/$version-$date-$hash/;
 #print $text;
 
 open(F,">$ARGV[0]/nsis/x2goclient.nsi") or die;
