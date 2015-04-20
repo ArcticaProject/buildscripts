46867d77cb55c76c9f7d7b64dad519c5bd0393cc
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2015-04-20 10:09:49 +0200

    fork X2Go buildscripts for Arctica Project

--- bin/mingw32-4.4+qt-4.8.wrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This script exists because it appears that you cannot pass env vars
-# to  C:\cygwin\bin\bash.exe from the windows command prompt.
-export NSIS_BUILD_FOR="mingw32-4.4:qt-4.8"
-$1 $2 $3 $4
